152eb6ac1fa70c304f0c1d6af07e87bdae4dc252
Scott Humphries <scott@sscotth.io>
2015-08-19 01:28:46 -0500

    first test

---
+++ app/home/routes.test.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var expect = require('chai').expect;
+var request = require('supertest');
+
+var app = require('../../app/');
+
+describe('Home Routes', function () {
+  describe('GET /', function(){
+    it('respond with index', function(done){
+      request(app)
+        .get('/')
+        .expect(200)
+        .end(function (err, res) {
+          expect(res.text).to.contain("I'm an index page");
+          done();
+        });
+    });
+  });
+
+  describe('GET /contact', function(){
+    it('respond with contact', function(done){
+      request(app)
+        .get('/contact')
+        .expect(200)
+        .end(function (err, res) {
+          expect(res.text).to.contain("I'm a contact page");
+          done();
+        });
+    });
+  });
+});
