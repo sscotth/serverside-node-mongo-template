d6c087ebc0197059a7d6e08635bb399bfacc76e3
Scott Humphries <scott@sscotth.io>
2015-08-19 09:05:10 -0500

    supertest requires a thrown error

--- app/home/routes.test.js
+++ app/home/routes.test.js
@@ -12,6 +12,7 @@ describe('Home Routes', function () {
         .get('/')
         .expect(200)
         .end(function (err, res) {
+          if (err) throw err;
           expect(res.text).to.contain("I'm an index page");
           done();
         });
@@ -24,6 +25,7 @@ describe('Home Routes', function () {
         .get('/contact')
         .expect(200)
         .end(function (err, res) {
+          if (err) throw err;
           expect(res.text).to.contain("I'm a contact page");
           done();
         });
